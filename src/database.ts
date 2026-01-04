7f74d835dc4cd8a6d24a7d82a78804b7b1c1c56c
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-01-04 17:52:08 -0300

    :closed_lock_with_key: feat: Env file

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config'
 import knex from 'knex'
 import type { Knex } from 'knex'
 
 export const knexConfig: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
