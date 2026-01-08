60182ee6534a135bd081618f30e532bbf019a0eb
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-01-08 00:40:07 -0300

    :rocket: chore: Prepare to deployment

--- src/database.ts
+++ src/database.ts
@@ -4,10 +4,12 @@ import type { Knex } from 'knex'
 import { env } from './env.js'
 
 export const knexConfig: Knex.Config = {
-  client: 'sqlite3',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === 'sqlite3' 
+    ? {
+      filename: env.DATABASE_URL,
+    } 
+    : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
